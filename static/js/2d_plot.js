77c7c3c100c87b6bc822fb90e9ec9ebb98017711
schaefre <rene.schaefer@rwth-aachen.de>
2020-08-25 18:32:41 +0200

    Fix canvas size

--- static/js/2d_plot.js
+++ static/js/2d_plot.js
@@ -1,45 +1,40 @@
-// Usage:
-canvas = document.getElementById('2d-canvas');
-console.log(canvas.width);
-console.log(canvas.height);
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-console.log(canvas.width);
-console.log(canvas.height);
-console.log($('#leftDiv').width());
-console.log($('#leftDiv').height());
-drawLineWithArrows(canvas,0,0,50,50,2,5,false,true);
+$(document).ready(function() {
+    canvas = document.getElementById('2d-canvas');
+    leftDiv = $('#leftDiv');
+    canvas.width = leftDiv.width();
+    canvas.height = leftDiv.height();
+    drawLineWithArrows(canvas,canvas.width/2,canvas.height/2,20,20,2,5,false,true);
 
-// x0,y0: the line's starting point
-// x1,y1: the line's ending point
-// width: the distance the arrowhead perpendicularly extends away from the line
-// height: the distance the arrowhead extends backward from the endpoint
-// arrowStart: true/false directing to draw arrowhead at the line's starting point
-// arrowEnd: true/false directing to draw arrowhead at the line's ending point
-
-function drawLineWithArrows(canvas, x0,y0,x1,y1,aWidth,aLength,arrowStart,arrowEnd){
-    ctx = canvas.getContext('2d');
-    var dx=x1-x0;
-    var dy=y1-y0;
-    var angle=Math.atan2(dy,dx);
-    var length=Math.sqrt(dx*dx+dy*dy);
-    //
-    ctx.translate(x0,y0);
-    ctx.rotate(angle);
-    ctx.beginPath();
-    ctx.moveTo(0,0);
-    ctx.lineTo(length,0);
-    if(arrowStart){
-        ctx.moveTo(aLength,-aWidth);
-        ctx.lineTo(0,0);
-        ctx.lineTo(aLength,aWidth);
-    }
-    if(arrowEnd){
-        ctx.moveTo(length-aLength,-aWidth);
+    // x0,y0: the line's starting point
+    // x1,y1: the line's ending point
+    // width: the distance the arrowhead perpendicularly extends away from the line
+    // height: the distance the arrowhead extends backward from the endpoint
+    // arrowStart: true/false directing to draw arrowhead at the line's starting point
+    // arrowEnd: true/false directing to draw arrowhead at the line's ending point
+    function drawLineWithArrows(canvas, x0,y0,x1,y1,aWidth,aLength,arrowStart,arrowEnd){
+        ctx = canvas.getContext('2d');
+        var dx=x1-x0;
+        var dy=y1-y0;
+        var angle=Math.atan2(dy,dx);
+        var length=Math.sqrt(dx*dx+dy*dy);
+        //
+        ctx.translate(x0,y0);
+        ctx.rotate(angle);
+        ctx.beginPath();
+        ctx.moveTo(0,0);
         ctx.lineTo(length,0);
-        ctx.lineTo(length-aLength,aWidth);
+        if(arrowStart){
+            ctx.moveTo(aLength,-aWidth);
+            ctx.lineTo(0,0);
+            ctx.lineTo(aLength,aWidth);
+        }
+        if(arrowEnd){
+            ctx.moveTo(length-aLength,-aWidth);
+            ctx.lineTo(length,0);
+            ctx.lineTo(length-aLength,aWidth);
+        }
+        //
+        ctx.stroke();
+        ctx.setTransform(1,0,0,1,0,0);
     }
-    //
-    ctx.stroke();
-    ctx.setTransform(1,0,0,1,0,0);
-}
+});
