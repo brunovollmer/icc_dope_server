9fda6b5788caaa59b34cd04a4475a336ffc42376
Bruno <vollmer.bruno@googlemail.com>
2020-08-25 11:08:12 +0200

    restructured js files

--- static/js/web_rtc.js
+++ static/js/web_rtc.js
@@ -1,32 +1,3 @@
-$(document).ready(function() {
-    $("#video_form").submit(function(e) {
-        $("#loader").css("display", "block");
-        $("#loading_overlay").css("display", "block");
-        e.preventDefault();
-        var form = $("#video_form")[0]
-        var formData = new FormData(form);
-        $.ajax({
-            type: "POST",
-            url: "/video",
-            processData: false,
-            contentType: false,
-            data: formData,
-            success: function(msg) {
-                console.log(msg);
-
-                $("#loader").css("display", "none");
-                $("#loading_overlay").css("display", "none");
-            },
-            error: function(msg) {
-                console.log('failure');
-
-                $("#loader").css("display", "none");
-                $("#loading_overlay").css("display", "none");
-                alert("FEHLER!")
-            }
-        });
-    })
-})
 
 // get DOM elements
 var dataChannelLog = document.getElementById('data-channel'),
@@ -40,13 +11,6 @@ var pc = null;
 // data channel
 var dc = null, dcInterval = null;
 
-document.querySelector("input[type=file]").onchange = function(event) {
-    console.log("test");
-    document.getElementById('media').style.display = 'block';
-  let file = event.target.files[0];
-  let blobURL = URL.createObjectURL(file);
-  document.querySelector("video").src = blobURL;
-}
 
 function createPeerConnection() {
     var config = {
