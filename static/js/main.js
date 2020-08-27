dfb232f714aa1fbafb1958ec93eb131dfad40204
schaefre <rene.schaefer@rwth-aachen.de>
2020-08-27 09:41:07 +0200

    Add video name to upload box

--- static/js/main.js
+++ static/js/main.js
@@ -139,4 +139,9 @@ $(document).ready(function() {
         //adjustCanvasSize();
         adjustPlotSize();
     });
+
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
 });
